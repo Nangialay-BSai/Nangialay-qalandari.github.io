bd0f76882b37cd8b2d49cd128802a47457b46d7d
Nangialay-BSai <nangialayhamidi65@gmail.com>
2025-08-29 19:19:38 +0500

    Delete login.bat

--- login.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Opening login page...
-start chrome simple-login.html
-if errorlevel 1 start firefox simple-login.html
-if errorlevel 1 start msedge simple-login.html
-if errorlevel 1 start simple-login.html
-pause
